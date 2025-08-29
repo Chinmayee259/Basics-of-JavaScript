c53920f520953cc5a6852662d629cc109abed7b5
Chinmayee259 <chinmayeeburte25@gmail.com>
2025-08-29 22:04:22 +0530

    Update Variables&DataTypes.js

--- Variables and Data Types/Variables&DataTypes.js
+++ Variables and Data Types/Variables&DataTypes.js
@@ -1,10 +1,22 @@
 //  ************  Variables and Data Types  ****************
 
-// Defining a variable with let keywordd
-let name = "Tony Starck";   //String
+//Rules for variables
+//1. Variable names are case sensitive; "a" & "A" is different 
+//2. Only letters, digits, underscore, and $ is allowed . Not even space
+//3. Only a letter, underscore or $ should be the 1st character
+//4. Reserve words cannot be variable names
+
+//Generally we use camel case for writing the variable name such as fullName, isFollow
+
+// Defining a variable with let keyword
+//1. var: Variable can be re-declared & updated.A global scope variable.
+//2. let: Variable cannot be re-decalres but can be updated. A block scope variable.
+//3. const: Variable cannot be re-declared or updated.A block scope variable.
+
+let name = "Tony Starck";   //Data Type: String
 console.log(name);  //Tony Stark
 
-let age = 24;   //numbers
+let age = 24;   //Data Type: numbers
 console.log(age);   
 //let age = 34 //error
 age = 30; //can update
@@ -31,3 +43,7 @@ console.log(Student["age"]);    //30
 console.log(Student["Name"]);   //Sanjay
 Student["age"] = Student["age"]+1;  
 console.log(Student["age"]);    //31
+
+//Different data types
+//Primary data types:  Number, Boolean, String, BigInt, Undefined, Null, Symbol
+//typeOf variable
